2d228d5b2202f037dd68b44f25c1a0630c507deb
StanislavBadyoukov <stanislav.badyoukov@mail.ru>
2022-02-24 23:40:50 +0300

    feat: add task solution

--- src/index.js
+++ src/index.js
@@ -38,9 +38,57 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
-}
 
+
+
+    let result = '';
+    
+    for (let i = 0; i < Math.floor(expr.length/10); i++)
+        { 	
+            let oneSymbol = expr.slice(i*10, i*10 + 10)
+            let firstOne = oneSymbol.indexOf("1")
+            let oneSymbolText = oneSymbol.slice(firstOne);
+            let textMorze = '';
+            
+            for (let j = 0; j < oneSymbolText.length; j += 2)
+                {	
+                
+            if (oneSymbolText === '*') {
+                    textMorze = `${textMorze}*`
+        
+                }
+    
+            else {
+                
+            
+                if (oneSymbolText.slice(j, j+2) === '11')
+                    {
+                    textMorze = `${textMorze}-`
+                    } 
+                    else if (oneSymbolText.slice(j, j+2) === '10') 
+                        {
+                        textMorze = `${textMorze}.`
+                        }
+                }
+            
+            }
+            
+            let decodeSymbol = MORSE_TABLE[textMorze]
+            
+            if (decodeSymbol == undefined) {
+                result = `${result} `
+            } else {
+                result = `${result}${decodeSymbol}`
+            }
+            
+        }
+    
+    
+        return result; 
+    
+    
+    }
+    
 module.exports = {
     decode
 }
